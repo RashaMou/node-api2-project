fdbbca6e22d46c6d89cf93dc32aa522e9a2b4360
Rasha Moumneh <rasha@fastmail.com>
2019-12-05 22:13:54 -0500

    Complete MVP

--- posts/postsRouter.js
+++ posts/postsRouter.js
@@ -17,6 +17,12 @@ router.get("/", (req, res) => {
 });
 
 router.post("/", (req, res) => {
+  const { title, contents } = req.body;
+  if (!title || !contents) {
+    res.status(400).json({
+      errorMessage: "Please provide title and contents for the post."
+    });
+  }
   Posts.insert(req.body)
     .then(post => {
       res.status(201).json({ ...post, ...req.body });
@@ -36,18 +42,76 @@ router.get("/:id", (req, res) => {
     });
 });
 
-router.delete("/:id", (req, res) => {
-  Posts.remove(req.params.id)
-    .then(count => {
-      if (count > 0) {
-        res.status(201).json(`${count} posts have been deleted`);
-      } else {
-        res.status(404).json("The post could not be found");
-      }
+router.get("/:id/comments", (req, res) => {
+  Posts.findPostComments(req.params.id)
+    .then(comments => {
+      res.status(200).json(comments);
     })
     .catch(error => {
-      res.status(500).json("Error deleting post");
+      res.status(500).json("Error retrieving comments");
     });
+});
+
+router.post("/:id/comments", async (req, res) => {
+  const text = await req.body;
+  const count = await res;
+  console.log(res);
+  try {
+    if (count == 0) {
+      res
+        .status(404)
+        .json({ message: "The post with the specified ID does not exist." });
+    }
+    if (!text) {
+      res.status(400).json({
+        errorMessage: "Please provide text for the comment."
+      });
+    } else Posts.insert(text);
+    res.status(201).json(text);
+  } catch (error) {
+    res.status(500).json("Error adding post");
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  const post = await Posts.findById(req.params.id);
+  const count = await Posts.remove(req.params.id);
+  try {
+    if (count > 0) {
+      res.status(201).json({ message: "Removed post:", post });
+    } else res.status(404).json({ message: "The post could not be found" });
+  } catch (error) {
+    res.status(500).json("Error deleting post");
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  const { title, contents } = req.body;
+
+  if (!title || !contents) {
+    res.status(400).json({
+      errorMessage: "Please provide title and contents for the post."
+    });
+  }
+
+  const count = await Posts.update(req.params.id, req.body);
+  console.log(count);
+  try {
+    if (count == 1) {
+      const updatedPost = await Posts.findById(req.params.id);
+      res
+        .status(201)
+        .json({ message: "Post updated successfully", updatedPost });
+    } else {
+      res
+        .status(500)
+        .json({ message: "The post with the specified ID does not exist." });
+    }
+  } catch {
+    res
+      .status(500)
+      .json({ message: "The post information could not be modified." });
+  }
 });
 
 module.exports = router;
